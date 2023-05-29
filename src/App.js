0563d32371f2c58b38277369643b52a07a34d8f4
Aman <amanprakashjanoriya@gmail.com>
2023-05-29 21:53:46 +0530

    29.05.2023

--- src/App.js
+++ src/App.js
@@ -1,29 +1,70 @@
-import React, { useState} from "react";
+import React, { useState,useEffect} from "react";
 import QrReader from 'modern-react-qr-reader'
-import "./style.css";
+// import "./style.css";
+import axios from 'axios';
+import {Button,Typography} from '@material-ui/core';
 
 export default function App() {
-  const [result, setResult] = useState("No result");
-  let handleScan = data => {
-    if (data) {
+  const [result, setResult] = useState(" ");
+  const[errorMessage,setErrorMessage]=useState(" ");
+  let handleScan =(data) => {
+    if (data && result !==data) {
       setResult(data);
     }
   };
 
   let handleError = err => {
-    console.error(err);
-    // alert(err);
+    setErrorMessage(err.message);
   };
+
+  useEffect(()=>{
+    const timeout=setTimeout(()=>setResult(''),5000);
+    return ()=>clearTimeout(timeout);
+  },[result]);
+
+  const submitQRData=async()=>{
+    try{
+      const response= await axios.post('http://[::1]/training/API/QrRead',{data:result});
+      console.log(response.data);
+      setResult('');
+      setErrorMessage('');
+    
+  }
+  catch(error){
+setErrorMessage('Failed to submit QR code data')
+  }
+}
   return (
     <div>
+      <Typography variant="h4" align='center'>QR Code Scanner</Typography>
       <QrReader
         delay={500}
         onError={handleError}
         onScan={handleScan}
-        style={{ width: "100%" }}
+        style={{ width: "100%",maxWidth:'400px',margin:'20px auto'}}
         facingMode="environment"
       />
-      <p>{result}</p>
+      {result && (
+        <div>
+          <Typography variant="h5" align='center'>Result:</Typography>
+          <Typography>{result}</Typography>
+        </div>
+      )}
+      {!result && (
+        <div>
+          <Typography variant="h5" align='center'>Please Scan QR Code Using the Camera Above </Typography>
+        </div>
+      )}
+      <Typography align='center'>
+      <Button  
+              variant="contained"
+              color="primary"
+       onClick={submitQRData}>Submit</Button>
+       </Typography>
+      {errorMessage && <p>{errorMessage}</p>}
+      
+
     </div>
   );
-}
+
+  }
